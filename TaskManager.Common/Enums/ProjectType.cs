f659c6f7620ef7e9b67efde9592dd6736a6daa82
Maryna Samsoniuk <m.samsonyuk@gmail.com>
2026-02-17 19:43:05 +0200

    Add ProjectDBModel and Common library with ProjectType enum

---
+++ TaskManager.Common/Enums/ProjectType.cs
@@ -0,0 +1,14 @@
+namespace TaskManager.Common.Enums
+{
+    public enum ProjectType
+    {
+        Educational,
+        Work,
+        Team,
+        Personal,
+        Commercial,
+        Academic,
+        Hobby,
+        Other
+    }
+}
--- TaskManager.DBModels/ProjectDBModel.cs
+++ TaskManager.DBModels/ProjectDBModel.cs
@@ -1,6 +1,25 @@
-namespace TaskManager.DBModels;
+using TaskManager.Common.Enums;
 
-public class ProjectDBModel
+namespace TaskManager.DBModels
 {
-    
+    public class ProjectDBModel
+    {
+        public Guid Id { get;}
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ProjectType ProjectType { get; set; }
+
+        private ProjectDBModel()
+        {
+            
+        }
+
+        public ProjectDBModel(string name, string description, ProjectType projectType)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            ProjectType = projectType;
+        }
+    }
 }
