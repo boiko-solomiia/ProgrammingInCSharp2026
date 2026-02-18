6c63a33b2b7ca1299541068d1876265f4b567e04
Solomiia Boiko <so.boiko@ukma.edu.ua>
2026-02-18 15:24:43 +0200

    Add TaskDBModel and Priority enum

---
+++ TaskManager.Common/Enums/Priority.cs
@@ -0,0 +1,12 @@
+namespace TaskManager.DBModels
+{
+    public enum Priority
+    {
+        Critical,
+        High,
+        Medium,
+        Low,
+        Backlog,
+        Other
+    }
+}
--- TaskManager.DBModels/TaskDBModel.cs
+++ TaskManager.DBModels/TaskDBModel.cs
@@ -1,7 +1,30 @@
+using TaskManager.Common.Enums;
+
 namespace TaskManager.DBModels
-{
+{ 
     public class TaskDBModel
     {
-    
+        public Guid Id { get; }
+        public Guid ProjectId { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TaskDBModel(Guid projectId, string name, string description, Priority priority, DateTime deadline, bool isCompleted) {
+            Id = Guid.NewGuid();
+            ProjectId = projectId;
+            Name = name;
+            Description = description;
+            Priority = priority;
+            Deadline = deadline;
+            IsCompleted = isCompleted;
+        }
+
+        public TaskDBModel(Guid projectId, string name, Priority priority, DateTime deadline, bool isCompleted)
+             : this(projectId, name, string.Empty, priority, deadline, isCompleted)
+        {
+        }
     }
 }
