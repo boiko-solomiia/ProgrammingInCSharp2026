6413c9307356b4e398aa03e66a3360143a78f918
Solomiia Boiko <so.boiko@ukma.edu.ua>
2026-02-18 18:47:39 +0200

    Add classes for creation, display and editing of Task entity in UIModels

--- TaskManager.UIModels/TaskUIModels/TaskCreateModel.cs
+++ TaskManager.UIModels/TaskUIModels/TaskCreateModel.cs
@@ -1,65 +1,60 @@
-using TaskManager.Common.Enums;
+﻿using TaskManager.Common.Enums;
 
 namespace TaskManager.UIModels.TaskUIModels
 {
-    public class TaskDisplayModel
+    public class TaskCreateModel
     {
-        private TaskDBModel? _dbModel;
+
         private Guid _projectId;
         private string _name;
         private string _description;
         private Priority _priority;
         private DateTime _deadline;
         private bool _isCompleted;
-        private bool _isOverdue;
 
-        public Guid Id
-        {
-            get => _dbModel.Id;
-        }
-        public Guid ProjectId 
+        public Guid ProjectId
         {
             get => _projectId;
         }
         public string Name
-        { 
+        {
             get => _name;
+            set => _name = value;
         }
         public string Description
         {
             get => _description;
+            set => _description = value;
         }
         public Priority Priority
         {
             get => _priority;
+            set => _priority = value;
         }
         public DateTime Deadline
         {
             get => _deadline;
+            set => _deadline = value;
         }
         public bool IsCompleted
         {
             get => _isCompleted;
+            set => _isCompleted = value;
         }
         public bool IsOverdue
         {
-            get
-            {
-                if (_isCompleted) return false;
-                return DateTime.UtcNow > _deadline;
-            }
+            get => _isOverdue;
         }
 
-        public TaskDisplayModel(TaskDBModel dbModel)
+        public TaskCreateModel(Guid projectId)
         {
-            _dbModel = dbModel;
-            _projectId = dbModel.ProjectId;
-            _name = dbModel.Name;
-            _description = dbModel.Description;
-            _priority = dbModel.Priority;
-            _deadline = dbModel.Deadline;
-            _isCompleted = dbModel.IsCompleted;
+            _projectId = projectId;
         }
 
+        public TaskDBModel ToDbModel()
+        {
+            return new TaskDBModel(_projectId, _name, _description, _priority, _deadline, _isCompleted);
+        }
     }
 }
+
