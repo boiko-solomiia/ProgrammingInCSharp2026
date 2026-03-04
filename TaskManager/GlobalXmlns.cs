9477c32b121d8b0a3b60587c01c3f58c0cd00bf3
Solomiia Boiko <so.boiko@ukma.edu.ua>
2026-03-05 00:18:48 +0200

    create TaskManager MAUI app

---
+++ TaskManager/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TaskManager")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TaskManager.Pages")]
