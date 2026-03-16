832f61d4c093bee840365512552a85c0201a75af
Solomiia Boiko <so.boiko@ukma.edu.ua>
2026-03-16 23:30:51 +0200

    Add projects and project details viewmodels

---
+++ TaskManager/ViewModels/ProjectDetailsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TaskManager.DTOModels.ProjectDTO;
+using TaskManager.DTOModels.TaskDTO;
+using TaskManager.Pages;
+using TaskManager.Services;
+
+namespace TaskManager.ViewModels
+{
+    public partial class ProjectDetailsViewModel : ObservableObject, IQueryAttributable
+    {
+        private readonly IProjectService _projectService;
+        private readonly ITaskService _taskService;
+
+        [ObservableProperty]
+        private ProjectDetailsDTO currentProject;
+
+        [ObservableProperty]
+        private ObservableCollection<TaskListDTO> tasks;
+
+        public ProjectDetailsViewModel(IProjectService projectService, ITaskService taskService)
+        {
+            _projectService = projectService;
+            _taskService = taskService;
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            var projectId = (Guid)query["ProjectId"];
+
+            CurrentProject = _projectService.GetProject(projectId);
+            Tasks = new ObservableCollection<TaskListDTO>(_taskService.GetTasksForProject(projectId));
+
+            OnPropertyChanged(nameof(Tasks));
+        }
+
+        [RelayCommand]
+        private void LoadTask(Guid taskId)
+        {
+            Shell.Current.GoToAsync(
+                $"{nameof(TaskDetailsPage)}",
+                new Dictionary<string, object>
+                {
+                    { "TaskId", taskId }
+                });
+        }
+    }
+}
---
+++ TaskManager/ViewModels/ProjectsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using TaskManager.DTOModels.ProjectDTO;
+using TaskManager.Pages;
+using TaskManager.Services;
+
+namespace TaskManager.ViewModels
+{
+    public class ProjectsViewModel
+    {
+        private readonly IProjectService _projectService;
+
+        public ObservableCollection<ProjectListDTO> Projects { get; set; }
+        public ProjectListDTO? SelectedProject { get; set; }
+        public Command ProjectSelectedCommand { get; }
+
+        public ProjectsViewModel(IProjectService projectService)
+        {
+            _projectService = projectService;
+
+            Projects = new ObservableCollection<ProjectListDTO>(_projectService.GetAllProjects());
+            ProjectSelectedCommand = new Command(LoadProject);
+        }
+
+        private void LoadProject()
+        {
+            if (SelectedProject == null)
+                return;
+
+            Shell.Current.GoToAsync($"{nameof(ProjectDetailsPage)}", new Dictionary<string, object>{ { "ProjectId", SelectedProject.Id }});
+        }
+    }
+}
